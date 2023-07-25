4f8e7afa142893a378a214febb8b98fd2cbcf307
Henrique Tavares <ihenrits@gmail.com>
2023-07-25 11:04:03 -0300

    chore: add size in TextInput

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ihenrits-ui/react
 
+## 2.2.2
+
+### Patch Changes
+
+- add size in TextInput
+
 ## 2.2.1
 
 ### Patch Changes
--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,14 +1,16 @@
 import { ComponentProps, forwardRef, ElementRef } from 'react';
 import { Input, Prefix, TextInputContainer } from './styles';
 
-export interface ITextInputProps extends ComponentProps<typeof Input> {
+export interface ITextInputProps
+  extends Omit<ComponentProps<typeof Input>, 'size'> {
   prefix?: string;
+  size?: ComponentProps<typeof TextInputContainer>['size'];
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, ITextInputProps>(
-  ({ prefix, ...props }: ITextInputProps, ref) => {
+  ({ prefix, size = 'md', ...props }: ITextInputProps, ref) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer size={size}>
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
       </TextInputContainer>
